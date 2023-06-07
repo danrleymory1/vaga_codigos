e4eee3ac0edb4396863ca362db8c119d6da2c1d3
danrleymory1 <61995815+danrleymory1@users.noreply.github.com>
2023-06-07 12:26:42 -0300

    Add files via upload

--- nPrimos_linear.py
+++ nPrimos_linear.py
@@ -4,4 +4,25 @@ por Danrley Ribeiro
 """
 
 
-def
+def check_num(n):
+    while n < 1:
+        return check_num(int(input("Valor inválido. Digite novamente: ")))
+    return n
+
+
+def primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+lista = []
+num = check_num(int(input()))
+for j in range(num+1):
+    if primo(j):
+        lista.append(j)
+
+print(f"p({num}) = {lista}")
--- nPrimos_recursivo.py
+++ nPrimos_recursivo.py
@@ -4,11 +4,29 @@ por Danrley Ribeiro
 """
 
 
-def check_n(n):
-    while n <= 0:
-        return check_n(int(input("Valor inválido. Digite novamente: ")))
+def check_num(n):
+    while n < 1:
+        return check_num(int(input("Valor inválido. Digite novamente: ")))
     return n
 
 
-def primo(n):
-    pass
+def primo(n, divisor=2):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % divisor == 0:
+        return False
+    if divisor * divisor > n:
+        return True
+
+    return primo(n, divisor + 1)
+
+
+lista = []
+num = int(input())
+for i in range(num+1):
+    if primo(i):
+        lista.append(i)
+
+print(f"p({num}) = {lista}")
